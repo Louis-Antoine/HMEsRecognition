2d9de5af061c952f5ec09c0828aafb8c0016c12f
kevtaur <kevtaur@users.noreply.github.com>
2022-04-01 17:59:30 -0400

    upload data
    uploaded original data to be modified

--- test.py
+++ test.py
@@ -16,14 +16,14 @@ trainingLoader = torch.utils.data.DataLoader(trainingSet, batch_size=64, shuffle
 valueLoader = torch.utils.data.DataLoader(valuesSet, batch_size=64, shuffle=True)
 dataiter = iter(trainingLoader)
 images, labels = dataiter.next()
-print(images.shape)
-print(labels.shape)
+# print(images.shape)
+# print(labels.shape)
 figure = plt.figure()
 num_of_images = 60
 for index in range(1, num_of_images + 1):
     plt.subplot(6, 10, index)
     plt.axis('off')
-    plt.imshow(images[index].permute(1,2,0).numpy(), cmap='gray_r')
+    plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
 plt.show()
 
 input_size = 45**2
@@ -36,15 +36,15 @@ model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
                       nn.ReLU(),
                       nn.Linear(hidden_sizes[1], output_size),
                       nn.LogSoftmax(dim=1))
-print(model)
+# print(model)
 
 criterion = nn.NLLLoss()
 images, labels = next(iter(trainingLoader))
-print(images.shape)
-print(labels.shape)
+# print(images.shape)
+# print(labels.shape)
 images = images.view(images.shape[0], -1)
-print(images.shape[0])
-print(labels)
+# print(images.shape[0])
+# print(labels)
 
 logps = model(images) #log probabilities
 loss = criterion(logps, labels) #calculate the NLL loss
@@ -73,5 +73,5 @@ for images, labels in trainingLoader:
         
         running_loss += loss.item()
 else:
-    print("Epoch {} - Training loss: {}".format(running_loss/len(trainingLoader)))
+    print("Epoch - Training loss: {}".format(running_loss/len(trainingLoader)))
 print("\nTraining Time (in minutes) =",(time()-time0)/60)
