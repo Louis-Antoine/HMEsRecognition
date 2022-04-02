63c9b85ed0811cbb633b8eb00e4ad40fe8edfdba
Louis-Antoine <60940273+Louis-Antoine@users.noreply.github.com>
2022-04-02 14:10:19 -0400

    Fixed inverted colours
    - added transforms.RandomInvert(1)

--- NumDataset.py
+++ NumDataset.py
@@ -7,7 +7,7 @@ from torchvision import datasets, transforms
 
 class NumbersDataset(Dataset):
     def __init__(self):
-        self.imgs_path = "extracted_images/"
+        self.imgs_path = "original_data/"
         file_list = glob.glob(self.imgs_path + "*")
         print(file_list)
         self.data = []
@@ -16,9 +16,9 @@ class NumbersDataset(Dataset):
             for img_path in glob.glob(class_path + "\*.jpg"):
                 self.data.append([img_path, class_name])
         print(self.data)
-        self.class_map = {"0" : 0, "1": 1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "-":10, "+":11, "=":12, "times":13}
+        self.class_map = {"0" : 0, "1": 1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "-":10, "+":11, "=":12, "times":13, "div": 14}
         self.img_dim = (45, 45)
-        self.transformations = transforms.Compose([transforms.ToPILImage(), transforms.Grayscale(num_output_channels=1), transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
+        self.transformations = transforms.Compose([transforms.ToPILImage(), transforms.Grayscale(num_output_channels=1), transforms.RandomInvert(1) ,transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
     def __len__(self):
         return len(self.data)
     def __getitem__(self, idx):
