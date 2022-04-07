7c23bce07b00cc3db52bf96377ab629f8fde224c
Louis-Antoine <60940273+Louis-Antoine@users.noreply.github.com>
2022-04-07 16:17:12 -0400

    Fixed image separator visualization

--- ImgSeparator.py
+++ ImgSeparator.py
@@ -101,12 +101,14 @@ answers = [0,1,2,3,4,5,6,7,8,9,'-','+','=','x','÷']
 #     axs[index].set_xlabel(predicted_digit)
 #     index+=1
 
-height = 2* len(tokens) +1
+height = 2* len(tokens) +3
 
-fig = plt.figure(figsize=(6,8))
+fig = plt.figure(figsize=(6,9))
 title = plt.subplot2grid((height,2), (0, 0), rowspan=2, colspan=2, xticklabels=[], yticklabels=[], xticks=[], yticks=[], fc="red",)
 title.imshow(image)
 title.set_title('Original Image')
+
+prediction = ''
 
 index = 2
 #plt.show()
@@ -114,8 +116,8 @@ for t in tokens:
     transform = transforms.Compose([transforms.Resize((45,45)),transforms.ToPILImage(), transforms.Grayscale(num_output_channels=1), transforms.RandomInvert(1) ,transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
     t = transform(t)
     img = t.view(1,len(t[0]),len(t[0][0]))
-    aximg = plt.subplot2grid((height, 2), (index, 0), rowspan=1, colspan=1, xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    axgraph = plt.subplot2grid((height, 2), (index, 1), rowspan=1, colspan=1)
+    aximg = plt.subplot2grid((height, 2), (index, 0), rowspan=2, colspan=1, xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    axgraph = plt.subplot2grid((height, 2), (index, 1), rowspan=2, colspan=1)
     
     aximg.imshow(img.numpy().squeeze(), cmap='gray_r')
 
@@ -127,6 +129,7 @@ for t in tokens:
 
     probab = list(ps.numpy()[0])
     top3 = (np.argsort(probab)[-3:])
+    print(top3)
     ps = ps.data.numpy().squeeze()
     print(np.where(ps == max(ps))[0])
     c = ['blue'] * 3
@@ -134,10 +137,20 @@ for t in tokens:
     c[0] = 'red'
     axgraph.barh(np.arange(3), bars, color=c)
     #ax2.set_aspect(max(ps))
-    axgraph.set_xticks(np.arange(3))
+    axgraph.set_xticks([0,0.5,1])
     symbols = [0,1,2,3,4,5,6,7,8,9,'-','+','=','x','÷']
-    axgraph.set_yticklabels([symbols[top3[2]], symbols[top3[1]], symbols[top3[0]]])
+    axgraph.set_yticks(np.arange(3),[symbols[top3[2]], symbols[top3[1]], symbols[top3[0]]] )
+    #axgraph.set_yticklabels([symbols[top3[2]], symbols[top3[1]], symbols[top3[0]]])
+    print(symbols[top3[2]])
+
+    prediction += str(symbols[top3[2]])
 
     index+=2
 
+axpred = plt.subplot2grid((height, 2), (index, 0), rowspan=1, colspan=2, xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+axpred.text(0.5, 0.5, prediction, horizontalalignment='center', verticalalignment='center')
+axpred.set_title('Prediction')
+
+title.set_label(prediction)
+plt.subplots_adjust(wspace=1, hspace=1)
 plt.show()
